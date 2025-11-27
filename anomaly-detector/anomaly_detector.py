ae1f214ecc7663c82dec9c4a98fc6024aaba46b5
naitikshah1008 <110518387+naitikshah1008@users.noreply.github.com>
2025-11-27 11:02:19 -0800

    change in anomaly detector

--- anomaly-detector/anomaly_detector.py
+++ anomaly-detector/anomaly_detector.py
@@ -43,30 +43,51 @@ def detect_anomaly(event):
     return None
 
 
+def create_consumer():
+    while True:
+        try:
+            consumer = KafkaConsumer(
+                INPUT_TOPIC,
+                bootstrap_servers=KAFKA_BROKER,
+                value_deserializer=lambda m: json.loads(m.decode("utf-8")),
+                auto_offset_reset="earliest",
+                enable_auto_commit=True,
+                group_id="rtm-anomaly-group"
+            )
+            print("Connected to Kafka (consumer)")
+            return consumer
+        except Exception as e:
+            print("Kafka (consumer) not ready yet, retrying...", str(e))
+            time.sleep(3)
+
+
+def create_producer():
+    while True:
+        try:
+            producer = KafkaProducer(
+                bootstrap_servers=KAFKA_BROKER,
+                value_serializer=lambda v: json.dumps(v).encode("utf-8")
+            )
+            print("Connected to Kafka (producer)")
+            return producer
+        except Exception as e:
+            print("Kafka (producer) not ready yet, retrying...", str(e))
+            time.sleep(3)
+
+
 def main():
-    consumer = KafkaConsumer(
-        INPUT_TOPIC,
-        bootstrap_servers=KAFKA_BROKER,
-        value_deserializer=lambda m: json.loads(m.decode("utf-8")),
-        auto_offset_reset="earliest",
-        enable_auto_commit=True,
-        group_id="rtm-anomaly-group"
-    )
-
-    producer = KafkaProducer(
-        bootstrap_servers=KAFKA_BROKER,
-        value_serializer=lambda v: json.dumps(v).encode("utf-8")
-    )
-
     print("Anomaly detector started")
+
+    consumer = create_consumer()
+    producer = create_producer()
 
     for msg in consumer:
         data = msg.value
-        result = detect_anomaly(data)
+        anomaly = detect_anomaly(data)
 
-        if result:
-            print("ANOMALY:", result)
-            producer.send(OUTPUT_TOPIC, result)
+        if anomaly:
+            print("ANOMALY:", anomaly)
+            producer.send(OUTPUT_TOPIC, anomaly)
 
 
 if __name__ == "__main__":
